735efc3f98c61719e4822b6cf88c9db0fe09c11f
CryptoD <dannylucas@zoho.com>
2018-01-29 22:23:10 +0100

    Day 3

--- 01_getting-started/02_numeral-systems/02_binary/main.go
+++ 01_getting-started/02_numeral-systems/02_binary/main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Binary: %b -- Hex: %x -- De: %d \n", 42, 42, 42)
+  fmt.Printf("Binary: %b -- Hex: %X -- De: %d \n", 42, 42, 42)
+  fmt.Printf("Binary: %#b -- Hex: %#x -- De: %#d \n", 42, 42, 42)
 }
