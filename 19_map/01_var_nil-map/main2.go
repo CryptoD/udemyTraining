794ba877a0d8f965da46fb265556a7b496deda20
Unknown <post.d@posteo.net>
2019-02-09 14:30:21 +0100

    Hash tables

--- 19_map/01_var_nil-map/main2.go
+++ 19_map/01_var_nil-map/main2.go
@@ -1,11 +1,15 @@
 package main
+
 import "fmt"
+
 func main() {
 
-m := make(map[int]int)
+	m := make(map[int]int)
 
-m[1] = 7
-m[2] = 2
-fmt.Println(m)
-
+	m[1] = 7
+	m[2] = 2
+	fmt.Println(m)
 }
+
+// Prints something like this:
+// map[1:7 2:2]
