7ea303267733eeae942adc059eefcf4a5d8761ca
Danny <post.d@posteo.net>
2020-02-16 18:59:23 +0100

    Sorting

---
+++ 21_interfaces/01_interface/01_no-interface/main2.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+type lake struct {
+  area int
+}
+
+func (depth lake) total() int {
+  return depth.area * depth.area
+}
+func main() {
+  x := lake{10}
+  fmt.Println("Total lake power is: ", x.total())
+}
