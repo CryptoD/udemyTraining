54a817b56597b40212964a275d36723c0b35002f
Danny <post.d@posteo.net>
2020-03-01 20:52:12 +0100

    Another update

--- 21_interfaces/02_package-sort/03.go
+++ 21_interfaces/02_package-sort/03.go
@@ -3,7 +3,7 @@ import "fmt"
 import "sort"
 
 func main() {
-  n := []int{7, 4, 8, 2, 9, 19, 12, 32, 3}
+  n := []int{7, 4, 8, 2, 9, 19, 12, 32, 3, 70}
   sort.Sort(sort.IntSlice(n))
 fmt.Println(n)
 
